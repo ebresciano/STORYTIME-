05e55ecb5209ece414f20baf78b2218be5994ced
Eva <Bresciano>
2016-07-05 18:01:32 -0600

    users progress. UserController functions

---
+++ Storytime/Storytime/User.swift
@@ -0,0 +1,9 @@
+//
+//  User.swift
+//  Storytime
+//
+//  Created by Eva Marie Bresciano on 7/2/16.
+//  Copyright © 2016 Eva Bresciano. All rights reserved.
+//
+
+import Foundation
---
+++ Storytime/Storytime/UserController.swift
@@ -0,0 +1,9 @@
+//
+//  UserController.swift
+//  Storytime
+//
+//  Created by Eva Marie Bresciano on 7/5/16.
+//  Copyright © 2016 Eva Bresciano. All rights reserved.
+//
+
+import Foundation
